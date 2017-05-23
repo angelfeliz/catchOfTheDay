7da99d89af273a667708a5983cd68f95f34a7b96
angelfeliz <erodriguezfeliz@gmail.com>
2017-05-23 13:42:54 -0400

    Updates

--- src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-   apiKey: "AIzaSyB6MUmasa-nOstxFnBZqQUj-LJy3rHF0bo",
-   authDomain: "catchoftheday-6c6af.firebaseapp.com",
-   databaseURL: "https://catchoftheday-6c6af.firebaseio.com",
-});
-
-export default base;
